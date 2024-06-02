f68d47e4f79efff0e0d1d72e2161b4471df5cda5
Jonian Guveli <jonian@hardpixel.eu>
2024-06-02 11:04:46 +0300

    Fix file util error

--- Source/Core/Common/FileUtil.cpp
+++ Source/Core/Common/FileUtil.cpp
@@ -899,8 +899,6 @@ const std::string& GetSysDirectory()
 void SetSysDirectory(const std::string& path)
 {
   INFO_LOG_FMT(COMMON, "Setting Sys directory to {}", path);
-  ASSERT_MSG(COMMON, s_sys_directory.empty(), "Sys directory already set to {}",
-             s_sys_directory);
   s_sys_directory = path + DIR_SEP;
 }
 
