d7a79cd8c02e0cddd55e93955a0bdc7038fbb302
Jonian Guveli <jonian@hardpixel.eu>
2025-11-30 13:48:58 +0200

    optimize libretro log listener

--- Source/Core/DolphinLibretro/Log.cpp
+++ Source/Core/DolphinLibretro/Log.cpp
@@ -20,18 +20,24 @@ private:
   retro_log_printf_t m_log;
 };
 
-static std::unique_ptr<LogListener> logListener;
-
 void Init()
 {
   struct retro_log_callback log = {};
   if (environ_cb(RETRO_ENVIRONMENT_GET_LOG_INTERFACE, &log) && log.log)
-    logListener = std::make_unique<LogListener>(log.log);
+  {
+    Common::Log::LogManager::GetInstance()->RegisterListener(
+      LogListener::CUSTOM_LISTENER,
+      std::make_unique<LogListener>(log.log)
+    );
+  }
 }
 
 void Shutdown()
 {
-  logListener.reset();
+  Common::Log::LogManager::GetInstance()->RegisterListener(
+    LogListener::CUSTOM_LISTENER,
+    nullptr
+  );
 }
 
 LogListener::LogListener(retro_log_printf_t log) : m_log(log)
@@ -46,20 +52,15 @@ LogListener::LogListener(retro_log_printf_t log) : m_log(log)
   Common::Log::LogManager::GetInstance()->SetEnable(Common::Log::LogType::DSPHLE, true);
   Common::Log::LogManager::GetInstance()->SetEnable(Common::Log::LogType::DSPLLE, true);
   Common::Log::LogManager::GetInstance()->SetEnable(Common::Log::LogType::DSP_MAIL, true);
-  Common::Log::LogManager::GetInstance()->EnableListener(LogListener::CONSOLE_LISTENER, false);
-  Common::Log::LogManager::GetInstance()->EnableListener(LogListener::CUSTOM_LISTENER, true);
 
-  Common::Log::LogManager::GetInstance()->RegisterListener(
-    LogListener::CUSTOM_LISTENER,
-    std::unique_ptr<Common::Log::LogListener>(logListener.get())
-  );
+  Common::Log::LogManager::GetInstance()->EnableListener(LogListener::CUSTOM_LISTENER, true);
+  Common::Log::LogManager::GetInstance()->EnableListener(LogListener::CONSOLE_LISTENER, false);
 }
 
 LogListener::~LogListener()
 {
   Common::Log::LogManager::GetInstance()->EnableListener(LogListener::CUSTOM_LISTENER, false);
   Common::Log::LogManager::GetInstance()->EnableListener(LogListener::CONSOLE_LISTENER, true);
-  Common::Log::LogManager::GetInstance()->RegisterListener(LogListener::CONSOLE_LISTENER, nullptr);
 }
 
 void LogListener::Log(Common::Log::LogLevel level, const char* text)
